26327cb0fbb5fab64dbae9cf749f7f56020a59bf
Markus Klinik <mkl@lambdanaut.net>
2020-11-23 17:09:48 +0100

    enable asynchronous handling
    if I understand correctly

--- src/bank/core.clj
+++ src/bank/core.clj
@@ -12,16 +12,18 @@
    :headers {"Content-type" "text/html"}
    :body (:remote-addr request)})
 
-(defn request-echo [request]
-  (do
+(defn request-echo
+  ([request respond raise] (respond (request-echo request)))
+  ([request]
     {:status 200
      :headers {"Content-type" "text/json"}
      ; the request body is a stream, we have to slurp it or else encode can't
      ; handle it. decode after slurp turns it into nested json
      :body (encode (update request :body (comp decode slurp)))}))
 
-(defn run []
+(defn run [async]
   (run-jetty
     request-echo
     {:port  3000
+     :async? async
      :join? false}))
