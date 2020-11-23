71a70cec40856ce28b2d00b96ecb1ffc91bc2b08
Markus Klinik <mkl@lambdanaut.net>
2020-11-23 15:30:29 +0100

    ring-jetty hello world

--- src/bank/core.clj
+++ src/bank/core.clj
@@ -1,6 +1,18 @@
-(ns bank.core)
+(ns bank.core
+  (:require [ring.adapter.jetty :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body "Hello World"})
+
+(defn what-is-my-ip [request]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body (:remote-addr request)})
+
+(defn run []
+  (run-jetty
+    what-is-my-ip
+    {:port  3000
+     :join? false}))
