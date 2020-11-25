a795c94f9f508d11cd5d3ba148699e25fd1edbfc
Markus Klinik <mkl@lambdanaut.net>
2020-11-25 10:44:29 +0100

    small refactoring: do and let not necessary

--- test/bank/database_test.clj
+++ test/bank/database_test.clj
@@ -15,24 +15,21 @@
 
 (deftest db-create-account-test
   (testing "create an account and see if it's there"
-    (let
-      [result (do
-        (drop-tables default-ds)
-        (create-tables default-ds)
-        (create-account default-ds "Mr. White")
-        (sql/execute! default-ds ["select * from account"]))]
+    (drop-tables default-ds)
+    (create-tables default-ds)
+    (create-account default-ds "Mr. White")
+    (let [result (sql/execute! default-ds ["select * from account"])]
       ; today I learned something new: map namespace syntax. The following two are equivalent:
       (is (= [{:account/id 1, :account/name "Mr. White", :account/balance 0.0}] result))
       (is (= [#:account{:id 1, :name "Mr. White", :balance 0.0}])))))
 
 (deftest db-get-account-test
   (testing "create some accounts and query them"
-    (do
-      (drop-tables default-ds)
-      (create-tables default-ds)
-      (create-account default-ds "Mr. White")
-      (create-account default-ds "Mr. Orange")
-      (create-account default-ds "Mr. Black"))
+    (drop-tables default-ds)
+    (create-tables default-ds)
+    (create-account default-ds "Mr. White")
+    (create-account default-ds "Mr. Orange")
+    (create-account default-ds "Mr. Black")
     (is (= [#:account{:id 1, :name "Mr. White", :balance 0.0}] (get-account default-ds 1)))
     (is (= [#:account{:id 2, :name "Mr. Orange", :balance 0.0}] (get-account default-ds 2)))
     (is (= [#:account{:id 3, :name "Mr. Black", :balance 0.0}] (get-account default-ds 3)))))
