70cb8f30ab5132a3c49b96a7ee24c2c69a6dbeb7
Markus Klinik <mkl@lambdanaut.net>
2020-12-08 07:41:08 +0100

    use test fixtures

--- haskell/test/DatabaseSpec.hs
+++ haskell/test/DatabaseSpec.hs
@@ -8,24 +8,20 @@ import Database
 import Types
 
 spec :: Spec
-spec = do
+spec = before_ (withConnection myConnectInfo reset) $ do
   describe "reset" $ do
     it "cretes an empty accounts table" $ do
-      withConnection myConnectInfo reset
       result <- withConnection myConnectInfo (\c -> query_ c "select * from account")
       result `shouldBe` ([] :: [(Int, String, Int)])
     it "cretes an empty accounts table, and parses to AccountInfo" $ do
-      withConnection myConnectInfo reset
       result <- withConnection myConnectInfo (\c -> query_ c "select * from account")
       result `shouldBe` ([] :: [AccountInfo])
 
   describe "createAccount" $ do
     it "cretes an account and returns it" $ do
-      withConnection myConnectInfo reset
       withConnection myConnectInfo (createAccount "Mr. Pink")
         `shouldReturn` [AccountInfo 1 "Mr. Pink" 0]
     it "cretes multiple accounts with ascending account number" $ do
-      withConnection myConnectInfo reset
       withConnection myConnectInfo (createAccount "Mr. Pink")
         `shouldReturn` [AccountInfo 1 "Mr. Pink" 0]
       withConnection myConnectInfo (createAccount "Mr. Orange")
@@ -33,13 +29,11 @@ spec = do
 
   describe "getAccount" $ do
     it "retrieves an account" $ do
-      withConnection myConnectInfo reset
       withConnection myConnectInfo (createAccount "Mr. Pink")
       withConnection myConnectInfo (getAccount 1)
         `shouldReturn` [AccountInfo 1 "Mr. Pink" 0]
 
     it "retrieves multiple accounts" $ do
-      withConnection myConnectInfo reset
       sequence_ [withConnection myConnectInfo (createAccount name)
         | name <- ["Mr. Orange", "Mr. Pink", "Mr. Black", "Mr. White"]]
       withConnection myConnectInfo (getAccount 4)
@@ -52,7 +46,6 @@ spec = do
         `shouldReturn` [AccountInfo 1 "Mr. Orange" 0]
 
     it "retrieves a non-existing account" $ do
-      withConnection myConnectInfo reset
       withConnection myConnectInfo (createAccount "Mr. Pink")
       withConnection myConnectInfo (getAccount 42)
         `shouldReturn` []
